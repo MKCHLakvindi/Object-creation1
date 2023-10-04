1401c724d9c1953b9860e4a4730b8cd99e0c9941
DELL <40-bis-0020@kdu.ac.lk>
2023-10-04 18:10:56 +0530

    first commit

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+class Student {
+    String name1="Sam";
+    String name2 ="John";
+
+    public static void main(String[] args) {
+        Student obj1 = new Student();
+        Student obj2 = new Student();
+        System.out.println(obj1.name1);
+        System.out.println(obj2.name2);
+    }
+}
